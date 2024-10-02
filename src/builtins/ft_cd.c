f709479d21c4d0fae9644c4ea94c1f599c10d58d
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2024-10-02 19:37:22 +0200

    casi el CD

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:00:46 by user              #+#    #+#             */
-/*   Updated: 2024/10/02 14:24:25 by descamil         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:36:00 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,84 +14,127 @@
 
 static int	ft_witch_dot(char *slash)
 {
-	if ((ft_strnstr(slash, "..", 3) && ft_strlen(slash) == 3)
-		|| (ft_strnstr(slash, ".", 2) && ft_strlen(slash) == 2))
+	if ((ft_strnstr(slash, "..", 2) && ft_strlen(slash) == 2) || (ft_strnstr(slash, ".", 1) && ft_strlen(slash) == 1))
+		return (2);
+	if ((ft_strnstr(slash, "..", 3) && ft_strlen(slash) == 3) || (ft_strnstr(slash, ".", 2) && ft_strlen(slash) == 2))
 		return (1);
 	return (0);
 }
 
-static int	ft_has_dots(char *str)
-{
-	char	*slash;
-	int		i;
+// static int	ft_has_dots(char *str)
+// {
+// 	char	*slash;
+// 	int		i;
 	
-	i = 0;
-	slash = ft_strrchr(str, '/');
-	while (ft_strlen(slash) != ft_strlen(str))
-	{
-		printf("SLASH --> %s\n", slash);
-		if (ft_witch_dot(slash) == 1)
-			i++;
-		*slash = '\0';
-		slash = ft_strrchr(str, '/');
-	}
-	return (i);
-}
+// 	i = 0;
+// 	slash = ft_strrchr(str, '/');
+// 	while (ft_strlen(slash) != ft_strlen(str))
+// 	{
+// 		if (ft_witch_dot(slash) == 1)
+// 			i++;
+// 		*slash = '\0';
+// 		slash = ft_strrchr(str, '/');
+// 	}
+// 	return (i);
+// }
 
 static void	ft_remove_dots(char **var)
 {
 	char	**string;
 	char	*dup;
-	int		dots;
 	int		len;
 	int		i;
 	int		j;
 
 	i = 0;
 	dup = ft_strdup(*var);
-	dots = ft_has_dots(dup);
-	printf(B_OR_0"DOTS --> %d\n"RESET, dots);
 	free(dup);
 	string = ft_split(*var, '/');
-	ft_strstr_printf(string);
 	len = ft_strstr_len(string);
 	while (string[i])
 	{
-		if (ft_witch_dot(string[i]) == 1)
+		if (ft_witch_dot(string[i]) == 2 && ft_strstr_len(string) == 1)
 		{
+			printf("%s\n", *string);
+			string[0] = NULL;
+			string[0] = ft_strdup("/");
+			break ;
+		}
+		// printf("[%s]\n", string[i]);
+		if (ft_witch_dot(string[i]) == 2)
+		{
+			// printf("%s\n", string[i]);
+			if (i - 1 == 0)
+			{
+				// printf("Aqui2\n");
+				string[i - 1] = ft_strdup("/");
+			}
 			if ((i - 1) >= 0)
 			{
-				j = i - 1;
+				j = i;
 				while (j >= 0 && string[j] == NULL)
 					j--;
-				if (j >= 0)
-					string[j] = NULL;
-				string[i] = NULL;
+				// printf("%d\n", j);
+				if (j - 1 > 0)
+					string[j - 1] = NULL;
+				if (j - 1 == 0)
+				{
+					// printf("Aqui\n");
+					string[j - 1] = ft_strdup("/");
+				}
+				string[j] = NULL;
 			}
 		}
 		i++;
 	}
 	i = 0;
-	int	rest = 0;
+	int		rest = 0;
 	while (len != 0)
 	{
-		if (string[i++] == NULL)
+		if (string[i] != NULL)
 			rest++;
+		i++;
 		len--;
 	}
-	char	**result;
+	char	*result;
+	char	*tmp;
+	char	*tmp2;
 
-	result = ft_calloc(sizeof(char *), rest + 1);
-	len = ft_strstr_len(string);
-	rest = 0;
-	while (len != 0)
+
+	result = NULL;
+	ft_strstr_printf(string);
+	if (string[0][0] == '/')
 	{
-		if (string[i++] != NULL)
-			result[rest++] = ft_strdup(string[i]);
-		len--;
+		free(*var);
+		result = ft_strdup("/");
+		*var = ft_strdup(result);
+		free(result);
+		return ;
 	}
-	printf("LEN --> %d\n"RESET, len);
-	*var = result;
+	tmp = ft_strdup("/");
+	tmp2 = ft_strdup("");
+	i = 0;
+	while (rest != 0)
+	{
+		printf("%s\n", string[i]);
+		if (string[i] != NULL)
+		{
+			if (result)
+				free(result);
+			tmp2 = ft_strjoin(string[i], "/");
+			result = ft_strjoin(tmp, tmp2);
+			free(tmp2);
+			if (tmp)
+				free(tmp);
+			tmp = ft_strdup(result);
+			rest--;
+		}
+		i++;
+	}
+	free(*var);
+	printf("RESULT --> %s\n", result);
+	*var = ft_strdup(result);
+	free(result);
 }
 
 // static char	*ft_save_home(t_mini *mini)
