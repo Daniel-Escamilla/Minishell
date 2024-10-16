2a25d27585112edf829f1426cf03c49f062efb5b
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2024-10-16 17:35:34 +0200

    Prueba de error de comando en hijos

--- src/executor/ft_comm_exec.c
+++ src/executor/ft_comm_exec.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 21:11:38 by user              #+#    #+#             */
-/*   Updated: 2024/10/15 19:46:09 by descamil         ###   ########.fr       */
+/*   Updated: 2024/10/16 13:12:00 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,7 @@ void	ft_comm_part1(t_cmd *cmd, t_mini *mini)
 			ft_perror_exit("Pipe Error", 1);
 	}
 	ft_open_fd(cmd, mini);
-	if (cmd->cmd == NULL)
-	{
-		if (cmd->args && cmd->args[0] && ft_nothing(cmd->args[0], 0) == 0
-			&& ft_is_dir(cmd->args[0]) == 0)
-			ft_printf_exit(cmd->args[0], ": command not found\n", 127);
-		if (cmd->type)
-			ft_protect_close_in_out(cmd);
-	}
-	if (cmd->cmd == NULL || (cmd->names->fd_infile < 0
-			|| cmd->names->fd_outfile < 1))
+	if (cmd->names->fd_infile < 0 || cmd->names->fd_outfile < 1)
 		mini->error = -4;
 	else
 		mini->error = 0;
@@ -84,13 +75,31 @@ void	ft_comm(t_cmd *cmd, t_mini *mini)
 	if (mini->single == 1 && cmd->built == 1)
 		g_exit_status = ft_exec_built(mini, cmd);
 	else if (cmd->names->fd_infile != -1 && cmd->names->fd_outfile != -1
-		&& cmd->files->error != -3 && mini->error != -4)
+		&& mini->error != -4)
 	{
+		// printf("%d\n", mini->num_comm);
 		mini->proc[mini->index] = fork();
 		if (mini->proc[mini->index] == -1)
 			ft_perror_exit("Failed in Fork()", 1);
 		if (mini->proc[mini->index] == 0)
+		{
+			if (cmd->cmd == NULL)
+			{
+				if (cmd->args && cmd->args[0] && ft_nothing(cmd->args[0], 0) == 0
+					&& ft_is_dir(cmd->args[0]) == 0)
+					dprintf(2, "%s: command not found\n", cmd->args[0]);
+					// ft_printf_exit(cmd->args[0], ": command not found\n", 127);
+				if (cmd->type)
+					ft_protect_close_in_out(cmd);
+				ft_close_and_update_fds(mini, cmd, 'H');
+				if (mini->fd_tmp != -1)
+					close (mini->fd_tmp);
+				mini->fd_tmp = -1;
+				close (mini->fd_history);
+				exit(1);
+			}
 			ft_comm_part2(cmd, mini);
+		}
 	}
 	ft_close_and_update_fds(mini, cmd, 'P');
 	if (cmd->names->join)
