18a9546c3d5f8db1d59239864732ce012ad7ce17
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2024-11-06 16:24:18 +0100

    Norminette files

---
+++ src/parser/ft_do_comm.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_do_comm.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 17:52:46 by smarin-a          #+#    #+#             */
+/*   Updated: 2024/10/31 16:08:49 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/parser.h"
+
+int	ft_order_all(t_mini *mini, t_cmd **cmd, char **lines, char *input)
+{
+	if (mini->flags->pipe == 0)
+	{
+		*(cmd) = ft_add_command(input, -1);
+		ft_move_files(*(cmd), mini);
+	}
+	else
+		process_lines(cmd, mini, lines, 0);
+	ft_do_expander(mini, *cmd);
+	ft_strstr_free(lines);
+	if (mini->cmd->files->error == -1)
+		return (-1);
+	ft_quit_spaces(cmd);
+	ft_do_remove_quotes(*cmd);
+	ft_remove_files(*cmd, mini);
+	if (mini->cmd->files->error == -1)
+		return (-1);
+	ft_select_files(*cmd, 0);
+	print_cmd(*cmd);
+	return (0);
+}
+
+static void	ft_fill_fd(t_mini *mini, t_cmd *cmd)
+{
+	t_cmd	*current;
+	int		fd;
+	int		i;
+
+	fd = -1;
+	current = cmd;
+	while (current != NULL)
+	{
+		i = 0;
+		while (cmd->files->order && cmd->files->order[i] != NULL)
+		{
+			cmd->error = 2;
+			if (ft_atoi(cmd->files->order[i]) == 2)
+				fd = ft_handle_trunc(cmd, mini, i);
+			else if (ft_atoi(cmd->files->order[i]) == 4)
+				fd = ft_handle_append(cmd, mini, i);
+			cmd->error = 0;
+			i++;
+			safe_close(&fd);
+		}
+		current = current->next;
+	}
+}
+
+static void	ft_start_comm_val(t_mini *mini)
+{
+	mini->index = 0;
+	mini->fd_tmp = -1;
+	mini->num_comm = mini->flags->pipe + 1;
+	mini->proc = ft_calloc(sizeof(pid_t), (size_t)mini->num_comm + 1);
+	if (mini->num_comm == 1)
+		mini->single = 1;
+}
+
+int	ft_do_comm(t_cmd *cmd, t_mini *mini)
+{
+	t_cmd	*current;
+
+	current = cmd;
+	
+	ft_start_comm_val(mini);
+	ft_fill_fd(mini, cmd);
+	while (current != NULL)
+	{
+		ft_start_val(current);
+		ft_comm(current, mini);
+		if (g_exit_status == 142)
+			return (-1);
+		mini->index++;
+		mini->num_comm--;
+		current = current->next;
+	}
+	ft_wait_bonus(mini);
+	return (0);
+}
+
+
--- src/parser/ft_strtok.c
+++ src/parser/ft_strtok.c
@@ -1,88 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strtok.c                                        :+:      :+:    :+:   */
+/*   ft_strtokc                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/01 17:52:46 by smarin-a          #+#    #+#             */
-/*   Updated: 2024/10/24 11:41:12 by descamil         ###   ########.fr       */
+/*   Created: 2024/10/29 09:18:37 by descamil          #+#    #+#             */
+/*   Updated: 2024/10/31 16:07:39 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parser.h"
-
-static int	ft_order_all(t_mini *mini, t_cmd **cmd, char **lines, char *input)
-{
-	if (mini->flags->pipe == 0)
-	{
-		*(cmd) = ft_add_command(input, -1);
-		ft_move_files(*(cmd), mini);
-	}
-	else
-		process_lines(cmd, mini, lines, 0);
-	ft_do_expander(mini, *cmd);
-	ft_strstr_free(lines);
-	if (mini->cmd->files->error == -1)
-		return (-1);
-	ft_quit_spaces(cmd);
-	ft_do_remove_quotes(*cmd);
-	ft_remove_files(*cmd, mini);
-	if (mini->cmd->files->error == -1)
-		return (-1);
-	ft_select_files(*cmd, 0);
-	return (0);
-}
-
-static void	ft_fill_fd(t_mini *mini, t_cmd *cmd)
-{
-	t_cmd	*current;
-	int		fd;
-	int		i;
-
-	fd = -1;
-	current = cmd;
-	while (current != NULL)
-	{
-		i = 0;
-		while (cmd->files->order && cmd->files->order[i] != NULL)
-		{
-			cmd->error = 2;
-			if (ft_atoi(cmd->files->order[i]) == 2)
-				fd = ft_handle_trunc(cmd, mini, i);
-			else if (ft_atoi(cmd->files->order[i]) == 4)
-				fd = ft_handle_append(cmd, mini, i);
-			cmd->error = 0;
-			i++;
-			safe_close(&fd);
-		}
-		current = current->next;
-	}
-}
-
-static int	ft_do_comm(t_cmd *cmd, t_mini *mini)
-{
-	t_cmd	*current;
-
-	current = cmd;
-	mini->index = 0;
-	mini->fd_tmp = -1;
-	mini->num_comm = mini->flags->pipe + 1;
-	mini->proc = ft_calloc(sizeof(pid_t), (size_t)mini->num_comm + 1);
-	if (mini->num_comm == 1)
-		mini->single = 1;
-	ft_fill_fd(mini, cmd);
-	while (current != NULL)
-	{
-		ft_start_val(current);
-		ft_comm(current, mini);
-		mini->index++;
-		mini->num_comm--;
-		current = current->next;
-	}
-	ft_wait_bonus(mini);
-	return (0);
-}
 
 int	ft_strtok(t_mini *mini, t_cmd **cmd, char *input)
 {
