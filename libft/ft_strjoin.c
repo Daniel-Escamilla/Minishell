cee74f796fc7135e149bcc5e2cf8d954611f0541
Daniel Escamilla <daniel.escamilla.bq@gmail.com>
2024-12-04 14:38:49 +0100

    Upss

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 17:01:27 by sergio            #+#    #+#             */
-/*   Updated: 2024/11/27 17:05:21 by descamil         ###   ########.fr       */
+/*   Updated: 2024/12/04 14:36:34 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ptr = ft_calloc(n + 1, 1);
 	if (ptr == NULL)
 		return (NULL);
-	i++;
-	while ((char)s1[++i] != '\0')
+	while ((char)s1[i] != '\0')
+	{
 		ptr[i] = (char)s1[i];
+		i++;
+	}
 	while ((char)s2[j] != '\0')
 		ptr[i++] = (char)s2[j++];
 	ptr[i] = '\0';
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 13:09:26 by sergio            #+#    #+#             */
-/*   Updated: 2024/11/27 17:04:49 by descamil         ###   ########.fr       */
+/*   Updated: 2024/12/04 14:37:42 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	src_len = ft_strlen(src);
 	if (dstsize > 0)
 	{
-		while (i < dstsize - 1 && src[i] && i++ > 0)
+		while (i < dstsize - 1 && src[i])
+		{
 			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
 	}
 	return (src_len);
