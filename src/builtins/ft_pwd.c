1d5d9ad8184a3814e7c83f425a94769ddf795ffa
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-02-22 12:13:03 +0100

    Refactor ft_pwd to write output directly to STDOUT

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,21 +6,21 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:53:53 by descamil          #+#    #+#             */
-/*   Updated: 2024/11/23 11:51:40 by descamil         ###   ########.fr       */
+/*   Updated: 2025/02/22 11:27:31 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 
-int	ft_pwd(t_cmd *cmd)
+int	ft_pwd(void)
 {
 	char	*pwd;
 
 	pwd = getcwd(NULL, 0);
 	if (pwd == NULL)
 		return (1);
-	write(cmd->names->fd_outfile, pwd, ft_strlen(pwd));
-	write(cmd->names->fd_outfile, "\n", 1);
+	write(STDOUT_FILENO, pwd, ft_strlen(pwd));
+	write(STDOUT_FILENO, "\n", 1);
 	free(pwd);
 	return (0);
 }
