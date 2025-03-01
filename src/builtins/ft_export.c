d62efc8276f1eada67abcea1456ff8384c845b4d
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-03-01 14:54:57 +0100

    Cambios en el export varias variables estan mal o bien. Corregido

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,39 +6,38 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 11:00:37 by descamil          #+#    #+#             */
-/*   Updated: 2025/02/27 15:42:36 by descamil         ###   ########.fr       */
+/*   Updated: 2025/03/01 14:53:32 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/builtins.h"
 
-int ft_find_var_value_export(char **env, char *arg, int quotes)
+int	ft_find_var_value_export(char **env, char *arg, int quotes)
 {
-    char    *str;
-    long    name_len;
-    int     i;
+	char	*str;
+	long	name_len;
+	int		i;
 
-    i = -1;
-    str = ft_strchr(arg, '=');
-    if (!str || ft_isdigit(arg[0]) == 1 || (str[0] == '\0' && quotes == 0))
-        return (-1);
-    name_len = str - arg;
-	if (g_exit_status == 1)
+	i = -1;
+	str = ft_strchr(arg, '=');
+	if (!str || ft_isdigit(arg[0]) == 1 || (str[0] == '\0' && quotes == 0))
 		return (-1);
-    while (env[++i])
-    {
-        if (ft_strncmp(env[i], arg, (size_t)name_len) == 0 && env[i][name_len] == '=')
-            return (i);
-    }
-    return (-2);
+	name_len = str - arg;
+	while (env[++i])
+	{
+		if (ft_strncmp(env[i], arg, (size_t)name_len) == 0
+			&& env[i][name_len] == '=')
+			return (i);
+	}
+	return (-2);
 }
 
-int ft_has_special_char(char *str)
+int	ft_has_special_char(char *str)
 {
 	int	i;
 
 	i = 0;
-	while(str && str[i] && str[i] != '=')
+	while (str && str[i] && str[i] != '=')
 	{
 		if (ft_is_special_char(str[i]) == 1)
 			return (1);
@@ -53,24 +52,27 @@ int	ft_export(t_mini *mini, t_cmd *cmd)
 	int		var;
 	int		i;
 
-	i = 1;
+	i = 0;
 	if (ft_strstr_len(cmd->args) == 1)
 		return (ft_print_order(mini->env->env));
-	while (cmd->args[i])
+	g_exit_status = 0;
+	while (cmd->args[++i])
 	{
-		if (cmd->args[i][0] == '=' || ft_has_special_char(cmd->args[i]) == 1)
+		if (cmd->args[i][0] == '=' || ft_has_special_char(cmd->args[i]) == 1
+			|| ft_isdigit(cmd->args[i][0]) == 1)
 		{
 			write(2, "mini: export: `", 16);
 			write(2, cmd->args[i], ft_strlen(cmd->args[i]));
 			write(2, "': not a valid identifier\n", 27);
 			g_exit_status = 1;
-			return (1);
+			continue ;
 		}
 		if (ft_strnstr(cmd->args[i], "OLDPWD", 6))
 			mini->oldpwd = 0;
 		if (ft_strnstr(cmd->args[i], "PATH", 4))
 			mini->path = 0;
-		var = ft_find_var_value_export(mini->env->env, cmd->args[i], ft_atoi(cmd->export_quotes[i]));
+		var = ft_find_var_value_export(mini->env->env, cmd->args[i],
+				ft_atoi(cmd->export_quotes[i]));
 		if (ft_strnstr(cmd->args[i], "_", 1) && ft_strlen(cmd->args[i]) == 1)
 			var = -1;
 		if (var != -1)
@@ -81,7 +83,6 @@ int	ft_export(t_mini *mini, t_cmd *cmd)
 			mini->env->env = ft_sindub_join(tmp, cmd->args[i]);
 			ft_strstr_free(tmp);
 		}
-		i++;
 	}
-	return (0);
+	return (g_exit_status);
 }
